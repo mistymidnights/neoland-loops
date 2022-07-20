743d90084439c454cbb7232bc7046dbe7a2fb2eb
mistymidnightss <mistydeveloping@gmail.com>
2022-07-20 19:15:52 +0200

    Iteración #7: For...of avanzado

--- README.md
+++ README.md
@@ -20,3 +20,7 @@ un ***console log*** el array.
 
 //Iteración #6: Mixed For...of e includes**
 Usa un bucle **for...of** para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes usar la función ***.includes()*** para comprobarlo.
+
+///*Iteración #7: For...of avanzado**
+    Usa un bucle **for...of** para recorrer todos los juguetes y añade los que tengan 
+    más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola..
--- loops.js
+++ loops.js
@@ -102,3 +102,24 @@ for (let index = 0; index < toys.length; index++) {
         }
 }
     console.log(toys)
+
+/*     **Iteración #7: For...of avanzado**
+    Usa un bucle **for...of** para recorrer todos los juguetes y añade los que tengan 
+    más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. Puedes usar este array */
+
+    const popularToys = [];
+    const toys2 = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+
+for (let index = 0; index < toys2.length; index++) {
+    const items = toys2[index];
+        if (items.sellCount > 15){
+            console.log(items)
+        }
+
+}
