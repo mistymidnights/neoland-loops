8514c639550ff2512687638f501e9cdac1781f78
mistymidnights <mistydeveloping@gmail.com>
2022-07-20 09:52:37 +0200

    save

--- loops.js
+++ loops.js
@@ -6,3 +6,60 @@ const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta
     for (let product of products) {
         console.log("INCLUDES STRING", product.includes("Camiseta"));
     }
+
+
+/*     **Iteración #2: Condicionales avanzados**
+
+Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad ***isApproved*** 
+a true o false en consecuencia. Una vez lo tengas compruébalo con un ***console.log***. */
+
+const alumns = [
+        {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+/* LO MIO */
+
+alumns.forEach((alumns) => {
+    let contador = 0;
+   if (alumns.T1 === true) {
+    contador = contador + 1;
+   }
+   /* TODO repetir */
+});
+
+console.log(alumns);
+
+/* **Iteración #3: Probando For...of**
+Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log*** sus valores. */
+
+/* const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+
+for (const places of placesToTravel) {
+}
+    console.log(placesToTravel);
+ */
+
+/* **Iteración #4: Probando For...in**
+Usa un **for...in** para imprimir por consola los datos del alienígena.. */
+
+/* const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+
+for (let items in alien) {
+        console.log(alien)
+    }
+ */
+
+/* **Iteración #5: Probando For**
+Usa un bucle **for** para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40. Imprime en 
+un ***console log*** el array. Puedes usar este array: */
+
+/* const placesToTravel2 = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}] */
